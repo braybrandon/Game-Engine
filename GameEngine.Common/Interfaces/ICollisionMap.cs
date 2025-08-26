d1245c94ffa850df8f00d4d476fc88f0e0345a5c
Brandon Bray <44496622+braybrandon@users.noreply.github.com>
2025-08-26 11:51:42 -0400

    fix(common): Add documentation to ICollisionMap (#210)

--- GameEngine.Common/Interfaces/ICollisionMap.cs
+++ GameEngine.Common/Interfaces/ICollisionMap.cs
@@ -2,24 +2,51 @@
 
 namespace GameEngine.Common.Interfaces
 {
+    /// <summary>
+    /// Represents a collision map for tile-based collision detection.
+    /// </summary>
     public interface ICollisionMap
     {
-        public int TileWidth
-        {
-            get;
-        }
+        /// <summary>
+        /// Gets the width of a single tile in pixels.
+        /// </summary>
+        int TileWidth { get; }
 
-        public int TileHeight { get; }
-        public int LayerWidth
-        {
-            get;
-        }
+        /// <summary>
+        /// Gets the height of a single tile in pixels.
+        /// </summary>
+        int TileHeight { get; }
 
-        public int LayerHeight { get; }
-        public bool IsSolid(Rectangle boundingBox);
+        /// <summary>
+        /// Gets the number of tiles horizontally in the layer.
+        /// </summary>
+        int LayerWidth { get; }
 
-        public bool IsSolid(Vector2 position);
+        /// <summary>
+        /// Gets the number of tiles vertically in the layer.
+        /// </summary>
+        int LayerHeight { get; }
 
-        public bool IsSolid(int x, int y);
+        /// <summary>
+        /// Determines if the specified bounding box collides with a solid tile.
+        /// </summary>
+        /// <param name="boundingBox">The bounding box to check.</param>
+        /// <returns>True if the bounding box collides with a solid tile; otherwise, false.</returns>
+        bool IsSolid(Rectangle boundingBox);
+
+        /// <summary>
+        /// Determines if the specified position collides with a solid tile.
+        /// </summary>
+        /// <param name="position">The position to check.</param>
+        /// <returns>True if the position collides with a solid tile; otherwise, false.</returns>
+        bool IsSolid(Vector2 position);
+
+        /// <summary>
+        /// Determines if the tile at the specified coordinates is solid.
+        /// </summary>
+        /// <param name="x">The x-coordinate of the tile.</param>
+        /// <param name="y">The y-coordinate of the tile.</param>
+        /// <returns>True if the tile is solid; otherwise, false.</returns>
+        bool IsSolid(int x, int y);
     }
 }
